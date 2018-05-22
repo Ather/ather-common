1a65870b0cdebcd96becad0bb89681126868a4c7
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-05-21 22:07:29 -0400

    Start work on new Configuration system

---
+++ ather-common-core/src/main/kotlin/com/atherapp/common/config/ConfigurationManager.kt
@@ -0,0 +1,65 @@
+package com.atherapp.common.config
+
+import kotlin.reflect.KClass
+
+/**
+ * Global configuration manager
+ *
+ * Configs are stored internally, and on the relevant file system (database or local)
+ * They are accessed with the [] operator with a string config name passed as a key.
+ * These should be unique for each section of the config used
+ */
+object ConfigurationManager {
+    //TODO Get this from the argument config of the main application
+    private val configDirectory = ""
+
+    init {
+        //TODO Load configs from configDirectory
+    }
+
+    /**
+     * Private map of configuration names and configs
+     */
+    private val configs: MutableMap<String, BaseConfiguration> = mutableMapOf()
+
+    /**
+     * Get the config relating to a name, optionally create new configs of the specified type.
+     * If non exists, return null
+     */
+    operator fun get(config: String, createNew: Boolean = true, newConfigClass: KClass<out BaseConfiguration>? = if (createNew) LocalConfiguration::class else null): BaseConfiguration? {
+        // Return the config from the internal list, otherwise return a new config if createNew is true and the config class isn't null
+        return configs[config] ?: return if (createNew && newConfigClass != null) return try {
+            // Try to invoke the first constructor of the config class
+            newConfigClass.constructors.first().call(config).also {
+                // Add the new config to the internal config map
+                configs[config] = it
+            }
+        } catch (e: Exception) {
+            // The first constructor isn't single-arg, or another exception occurred, so return null
+            null
+        } else null
+    }
+
+    /**
+     * Non-nullable equivalent to [get] which returns a new config of the specified name if one doesn't exist, or the config if it does.
+     */
+    operator fun get(config: String, newConfigClass: KClass<out BaseConfiguration> = LocalConfiguration::class): BaseConfiguration = this[config, true, newConfigClass]!!
+
+    /**
+     * Get a [UnionConfiguration] of all passed configs, meaning you can use them as one config
+     * This will not create new configurations if they don't exist
+     */
+    operator fun get(vararg configs: String) = UnionConfiguration(*configs.mapNotNull { this[it, false] }.toTypedArray())
+
+    /**
+     * Return a [UnionConfiguration] of all configs inside the manager
+     */
+    fun getAll() = UnionConfiguration(*configs.map { it.value }.toTypedArray())
+
+    val ARGUMENT_CONFIG
+        get() = this["argument"]
+
+    //TODO Come up with a few default configs
+    val DEFAULT_CONFIG
+        get() = this["default", LocalConfiguration::class]
+}
---
+++ ather-common-core/src/main/kotlin/com/atherapp/common/config/Configurations.kt
@@ -0,0 +1,165 @@
+package com.atherapp.common.config
+
+import com.github.salomonbrys.kotson.set
+import com.github.salomonbrys.kotson.toJson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+
+interface Configuration {
+    val configName: String
+
+    operator fun get(key: String): JsonElement?
+}
+
+interface PersistentConfiguration : Configuration {
+    fun load()
+
+    fun save()
+}
+
+/**
+ * A [BaseConfiguration] is designed for storing configurations inside JSON structures.
+ * It provides basic information about a given configuration, and allows the loading and saving
+ * of objects.
+ *
+ * It automatically calls the [save] function when a value is updated, and automatically calls
+ * [load] (and [loadJson]) when the constructor is called
+ *
+ * More complex objects can be stored inside the configuration.
+ *
+ * All implementing classes must have a single-arg constructor AS THE FIRST CONSTRUCTOR accepting only a config name to be properly supported by the [ConfigurationManager]
+ */
+abstract class BaseConfiguration: PersistentConfiguration {
+    abstract override val configName: String
+
+    protected lateinit var json: JsonObject
+
+    init {
+        this.load()
+    }
+
+    /**
+     * Get the JsonElement at the given key, or null if it doesn't exist
+     */
+    override operator fun get(key: String): JsonElement? = json[key]
+
+    /**
+     * Set the value at the given key to the nullable [JsonElement]
+     */
+    open operator fun set(key: String, value: JsonElement?) {
+        json[key] = value
+        this.save()
+    }
+
+    /**
+     * Retrieve the string representation of the Json from the persistence layer
+     */
+    protected abstract fun loadJson(): String
+
+    /**
+     * Call loadJson and set the internal [json] value
+     */
+    override fun load() {
+        this.json = this.loadJson().toJson().asJsonObject
+    }
+
+    /**
+     * Save the [json] value to the persistence location
+     */
+    abstract override fun save()
+}
+
+/**
+ * Combination of any number of underlying configurations
+ *
+ * This should only be used for reading values, and updating existing values only.
+ * By creating new values, they will end up in configs you possibly don't want.
+ */
+class UnionConfiguration(
+        vararg val configs: BaseConfiguration
+) : BaseConfiguration() {
+    override val configName: String = configs.joinToString("+") { it.configName }
+
+    /**
+     * Return the first matching value inside the union
+     */
+    override operator fun get(key: String): JsonElement? {
+        for (config in configs) {
+            val confVal = config[key]
+            if (confVal != null)
+                return confVal
+        }
+
+        return null
+    }
+
+    /**
+     * Return a list of all matching config values inside the union
+     */
+    fun getAll(key: String) = configs.mapNotNull { it[key] }
+
+    /**
+     * Update a value inside a config. This only works for updating values, not creating new values.
+     */
+    override operator fun set(key: String, value: JsonElement?) {
+        for (config in configs) {
+            val confVal = config[key]
+            if (confVal != null) {
+                config[key] = value
+                config.save()
+            }
+        }
+    }
+
+    /**
+     * Update or set a value inside a specified config.
+     */
+    operator fun set(configName: String, key: String, value: JsonElement?) {
+        val config = configs.find { it.configName == configName }
+        if (config != null) {
+            config[key] = value
+            config.save()
+        }
+
+    }
+
+    override fun loadJson(): String = ""
+
+    override fun load() {}
+
+    /**
+     * Save a specific config inside the union
+     */
+    fun save(configName: String) {
+        configs.find { it.configName == configName }?.save()
+    }
+
+    /**
+     * Save all configs in the union
+     */
+    override fun save() {
+        for (config in configs)
+            config.save()
+    }
+}
+
+/**
+ * Local configuration stored on the local filesystem.
+ * It is created by passing a relative path to the default config location of the running JVM instance
+ */
+class LocalConfiguration(
+        override val configName: String,
+        val relativePath: String = configName
+) : BaseConfiguration()
+
+/**
+ * Database configuration stored on a database system.
+ * To create one, you pass the ID/Name of a database profile configured on the system
+ *
+ * TODO Consider that this will have to assume a local configuration exists to get database info
+ */
+class DatabaseConfiguration(
+        override val configName: String,
+        //TODO Use default database profile
+        val databaseProfile: String = ConfigurationManager
+) : BaseConfiguration()
